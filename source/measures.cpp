2d6ee62e829fcf8bb723b91173a5aff1f0096737
ilaria <ilariam@kth.se>
2020-09-21 19:02:28 +0200

    source code

---
+++ source/measures.cpp
@@ -0,0 +1,187 @@
+//
+// Created by ilaria on 2019-11-16.
+//
+
+#include "measures.h"
+
+void energy(struct Measures &mis, struct H_parameters &Hp, double my_beta, struct Node* Site){
+
+    unsigned int i, ix, iy, iz, alpha, vec;
+    double h_Kinetic=0., h_Josephson=0., h_B=0., h_AB=0.;
+    double F_A=0;
+    double J_0, J_1, J_2;
+    double gauge_phase, gauge_phase_0,gauge_phase_1, gauge_phase_2;
+    double h2=(Hp.h*Hp.h);
+    double h3=(Hp.h*Hp.h*Hp.h);
+
+    for(ix=0; ix<Lx; ix++){
+        for(iy=0; iy<Ly; iy++){
+            for(iz=0; iz<Lz; iz++){
+                i=ix + Lx * (iy + iz * Ly);
+                for(alpha=0; alpha<3; alpha++) {
+                    //Kinetic= -(1/h²)*\sum_k=1,2,3 |Psi_{alpha}(r)||Psi_{alpha}(r+k)|* cos( theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r))
+                    for (vec = 0; vec < 3; vec++) {
+                        gauge_phase=Site[nn(i, vec, 1)].Psi[alpha].t - Site[i].Psi[alpha].t + Hp.h*Hp.e*Site[i].A[vec];
+                        //h_Kinetic -= (1. / h2) * O2prod(Site[i].Psi[alpha], NN_Site[i].Psi_plusk[alpha +3*vec]);
+                        h_Kinetic -= (1. / h2) * (Site[i].Psi[alpha].r*Site[nn(i, vec, -1)].Psi[alpha].r)*cos(gauge_phase) ;
+                    }
+                    for (vec = alpha+1; vec < 3; vec++) {
+                        //Josephson= eta* \sum_beta!=alpha |Psi_{alpha}(r)||Psi_{beys}(r)|* cos(theta_{alpha}(r) - theta_{beta}(r))
+                        h_Josephson += (Hp.eta * O2prod(Site[i].Psi[alpha], Site[i].Psi[vec]));
+                        if (Hp.e != 0) {
+                            //F_{alpha,vec}= A_alpha(r_i) + A_vec(ri+alpha) - A_alpha(r_i+vec) - A_vec(ri)
+                            F_A = (Site[i].A[alpha] + Site[nn(i, alpha, 1)].A[vec] - Site[nn(i, vec, 1)].A[alpha] -
+                                   Site[i].A[vec]);
+                            h_B += ((0.5 / h2) * (F_A * F_A));
+                        }
+                    }
+                }
+                //Andreev-Bashkin term = \sum_beta!=alpha \sum_k=1,2,3 -nu*(J^k_alpha *J^k_beta);
+                // with J^k_alpha= |Psi_{alpha}(r)||Psi_{alpha}(r+k)|* sin(theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r)))
+                if(Hp.nu >0 ) {
+                    for (vec = 0; vec < 3; vec++) {
+                        gauge_phase_0=Site[nn(i, vec, 1)].Psi[0].t - Site[i].Psi[0].t + Hp.h*Hp.e*Site[i].A[vec];
+                        gauge_phase_1=Site[nn(i, vec, 1)].Psi[1].t - Site[i].Psi[1].t + Hp.h*Hp.e*Site[i].A[vec];
+                        gauge_phase_2=Site[nn(i, vec, 1)].Psi[2].t - Site[i].Psi[2].t + Hp.h*Hp.e*Site[i].A[vec];
+                        J_0 = (1. / Hp.h) *(Site[i].Psi[0].r*Site[nn(i, vec, 1)].Psi[0].r)*sin(gauge_phase_0);
+                        J_1 = (1. / Hp.h) *(Site[i].Psi[1].r*Site[nn(i, vec, 1)].Psi[1].r)*sin(gauge_phase_1);
+                        J_2 = (1. / Hp.h) *(Site[i].Psi[2].r*Site[nn(i, vec, 1)].Psi[2].r)*sin(gauge_phase_2);
+                        h_AB -= Hp.nu * ( (J_0*J_1) + (J_0*J_2) + (J_1*J_2) );
+                    }
+                }
+            }
+        }
+    }
+
+    //to compute the heat capacity it is important to consider the total physical energy which is h_tot*h³
+    mis.E_kin=(double)h3*h_Kinetic;
+    mis.E_Josephson=(double)h3*h_Josephson;
+    mis.E_B= (double)h3*h_B;
+    mis.E_AB= (double)h3*h_AB;
+    mis.E=(mis.E_kin +mis.E_Josephson + mis.E_B + mis.E_AB);
+}
+
+
+void helicity_modulus(struct Measures &mis, struct H_parameters &Hp, struct Node* Site){
+
+    double J_alpha=0., J_beta=0., DJ_alpha_Dd=0., DJ_beta_Dd=0.;
+    unsigned int i, ix, iy, iz, alpha, beta, vec;
+    vec=0; //helicity modulus computed along the x direction
+    double gauge_phase1, gauge_phase2;
+
+    for(ix=0; ix<Lx;ix++){
+        for(iy=0; iy<Ly;iy++){
+            for(iz=0; iz<Lz;iz++){
+                i=ix +Lx*(iy+Ly*iz);
+                for(alpha=0; alpha<NC; alpha++){
+                    gauge_phase1=Site[nn(i, vec, 1)].Psi[alpha].t - Site[i].Psi[alpha].t + Hp.h*Hp.e*Site[i].A[vec];
+
+                    J_alpha=(1. / Hp.h)*(Site[i].Psi[alpha].r*Site[nn(i, vec, 1)].Psi[alpha].r)*sin(gauge_phase1);
+                    DJ_alpha_Dd=(1./Hp.h)*(Site[i].Psi[alpha].r*Site[nn(i, vec, 1)].Psi[alpha].r)*cos(gauge_phase1);
+
+                    mis.DH_Ddi[alpha] += ((1. / Hp.h) * J_alpha);
+                    mis.D2H_Dd2i[alpha]+= ((1. / Hp.h)*DJ_alpha_Dd ) ;
+
+                    for(beta=0; beta<NC; beta++) {
+                        if(beta !=alpha ){
+                            gauge_phase2=Site[nn(i, vec, 1)].Psi[beta].t - Site[i].Psi[beta].t + Hp.h*Hp.e*Site[i].A[vec];
+                            J_beta= (1./Hp.h)*(Site[i].Psi[beta].r*Site[nn(i, vec, 1)].Psi[beta].r)*sin(gauge_phase2);
+                            mis.DH_Ddi[alpha] -= ( Hp.nu *J_beta*DJ_alpha_Dd );
+                            mis.D2H_Dd2i[alpha]+=( Hp.nu *J_beta*J_alpha );
+                            DJ_beta_Dd=(1./Hp.h)*(Site[i].Psi[beta].r*Site[nn(i, vec, 1)].Psi[beta].r)*cos(gauge_phase2);
+                            mis.D2H_Dd2ij[alpha]+= (-Hp.nu*(DJ_alpha_Dd*DJ_beta_Dd));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+void dual_stiffness(struct Measures &mis, struct H_parameters &Hp, struct Node* Site){
+
+    double qx_min=C_TWO_PI/(Lx);
+    double invNorm= 1./((C_TWO_PI)*(C_TWO_PI)*N);
+    unsigned int i, ix, iy, iz;
+    double Re_rhoz=0.;
+    double Im_rhoz=0.;
+    double Dx_Ay, Dy_Ax;
+
+    for(ix=0; ix<Lx;ix++){
+        for(iy=0; iy<Ly;iy++){
+            for(iz=0; iz<Lz;iz++){
+                i=ix +Lx*(iy+Ly*iz);
+                Dx_Ay=(Site[nn(i, 0, 1)].A[1]- Site[i].A[1])/Hp.h;
+                Dy_Ax=(Site[nn(i, 1, 1)].A[0]- Site[i].A[0])/Hp.h;
+
+                Re_rhoz+=(cos((double)qx_min*ix)*(Dx_Ay -Dy_Ax));
+                Im_rhoz+=(sin((double)qx_min*ix)*(Dx_Ay -Dy_Ax));
+            }
+        }
+    }
+    mis.d_rhoz=invNorm*((Re_rhoz*Re_rhoz) +(Im_rhoz*Im_rhoz));
+}
+
+void magnetization(struct Measures &mis, struct Node* Site){
+    //The Ising parameter m(x,y)=+/-1 indicates the chirality of the three phases. If the phases are ordered as: phi_1, phi_2, phi_3 then m=1; otherwise if the order is phi_1, phi_3, phi_2 then m=-1.
+    unsigned ix, iy, iz, i, alpha;
+
+    std::vector <double> phi_shifted;
+    phi_shifted.resize(2,0.);
+
+    for(ix=0; ix<Lx;ix++) {
+        for (iy = 0; iy < Ly; iy++) {
+            for (iz = 0; iz < Lx; iz++) {
+                i=ix +Lx*(iy+Ly*iz);
+                for(alpha=1; alpha<3; alpha++){
+                    phi_shifted[alpha]=Site[i].Psi[alpha].t - Site[i].Psi[0].t;
+                    while(phi_shifted[alpha] >= C_TWO_PI){
+                        phi_shifted[alpha]-= C_TWO_PI;}
+                    while(phi_shifted[alpha]< 0){
+                        phi_shifted[alpha]+=C_TWO_PI;}
+                }
+                if(phi_shifted[1]>phi_shifted[2]){
+                    mis.m+=1;
+                }else if(phi_shifted[1]==phi_shifted[2]){
+		    mis.m+=0;	
+		}else if(phi_shifted[1]<phi_shifted[2]){
+                    mis.m+=(-1);
+                }
+            }
+        }
+    }
+    mis.m=mis.m/N;
+}
+
+
+void save_lattice(struct Node* Site, const fs::path & directory_write, std::string configuration){
+
+    std::string sPsi;
+    std::string sA;
+    sPsi= std::string("Psi_")+ configuration + std::string(".bin");
+    sA= std::string("A_")+ configuration + std::string(".bin");
+    fs::path psi_init_file = directory_write / sPsi;
+    fs::path a_init_file = directory_write / sA;
+
+    FILE *fPsi= nullptr;
+    FILE *fA= nullptr;
+    unsigned int i=0;
+
+    if((fPsi=fopen(psi_init_file.c_str(), "w"))) {
+        for (i = 0; i < N; i++) {
+            fwrite(Site[i].Psi, sizeof(struct O2), 3, fPsi);
+        }
+        fclose(fPsi);
+    }
+
+    if((fA=fopen(a_init_file.c_str(), "w"))) {
+    	for (i = 0; i < N; i++) {
+            fwrite(Site[i].A, sizeof(double), 3, fA);
+	}
+        fclose(fA);
+    }
+
+}
+
---
+++ source/montecarlo.cpp
@@ -0,0 +1,201 @@
+#include "montecarlo.h"
+#include "main.h"
+#include "rng.h"
+#include "class_tic_toc.h"
+
+void metropolis( struct Node* Site, struct MC_parameters &MCp, struct H_parameters &Hp,  double my_beta){
+
+    double d_theta, d_A, rand;
+    unsigned int ix, iy, iz, alpha, vec, i;
+    double acc_rate=0.5, acc_A=0., acc_theta=0.; // acc_rho=0.,
+    struct O2 NewPsi{};
+    struct O2 OldPsi{};
+    double NewA, OldA;
+    double newE, oldE, minus_deltaE;
+    double h3=(Hp.h*Hp.h*Hp.h);
+    class_tic_toc t_localHtheta(true,5,"local_Htheta");
+    class_tic_toc t_localHA(true,5,"local_HA");
+
+
+    for (ix= 0; ix < Lx; ix++) {
+        for (iy = 0; iy < Ly; iy++) {
+            for (iz = 0; iz < Lz; iz++) {
+                i = ix + Lx * (iy + iz * Ly);
+
+                /*******PHASE ONLY UPDATE**************/
+                for (alpha = 0; alpha < 3; alpha++) {
+                    OldPsi = Site[i].Psi[alpha];
+                    //t_localHtheta.tic();
+                    oldE = local_Htheta(OldPsi, ix, iy, iz, alpha, Hp, Site);
+                    //t_localHtheta.toc();
+                    d_theta = rn::uniform_real_box(-MCp.lbox_theta, MCp.lbox_theta);
+                    NewPsi.t = fmod(OldPsi.t + d_theta, C_TWO_PI);
+                    NewPsi.r = OldPsi.r;
+                    polar_to_cartesian(NewPsi);
+                    //t_localHtheta.tic();
+                    newE = local_Htheta(NewPsi, ix, iy, iz, alpha, Hp, Site);
+                    //t_localHtheta.toc();
+                    minus_deltaE = h3 * (oldE - newE);
+                    if (minus_deltaE > 0) {
+                        Site[i].Psi[alpha] = NewPsi;
+                        acc_theta++;
+                    } else {
+                        rand = rn::uniform_real_box(0, 1);
+                        //Boltzmann weight: exp(-\beta E) E= h³ \sum_i E(i)
+                        if (rand < exp(my_beta * minus_deltaE)) {
+                            Site[i].Psi[alpha] = NewPsi;
+                            acc_theta++;
+                        }
+                    }
+                }
+                if (Hp.e != 0) {
+                    /**********VECTOR POTENTIAL UPDATE********/
+                    for (vec = 0; vec < 3; vec++) {
+                        //Update of A
+                        OldA = Site[i].A[vec];
+                        //t_localHA.tic();
+                        oldE = local_HA(OldA, ix, iy, iz, vec, Hp, Site);
+                        //t_localHA.toc();
+                        d_A = rn::uniform_real_box(-MCp.lbox_A, MCp.lbox_A);
+                        NewA = OldA + d_A;
+                        //t_localHA.tic();
+                        newE = local_HA(NewA, ix, iy, iz, vec, Hp, Site);
+                        //t_localHA.toc();
+                        minus_deltaE = h3 * (oldE - newE);
+                        if (minus_deltaE > 0.) {
+                            Site[i].A[vec] = NewA;
+                            acc_A++;
+                        } else {
+                            rand = rn::uniform_real_box(0, 1);
+                            //Boltzmann weight: exp(-\beta E) E= h³ \sum_i E(i)
+                            if (rand < exp(my_beta * minus_deltaE)) {
+                                Site[i].A[vec] = NewA;
+                                acc_A++;
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+//    t_localHA.print_measured_time();
+ //   t_localHtheta.print_measured_time();
+ //   t_localHpsi.print_measured_time();
+    acc_theta=(double) acc_theta/(3*N);
+    acc_A=(double) acc_A/(3*N);
+    MCp.lbox_theta= MCp.lbox_theta*(acc_theta/acc_rate);
+    MCp.lbox_A= MCp.lbox_A*(acc_A/acc_rate);
+}
+
+
+double local_Htheta(struct O2 Psi, unsigned int ix, unsigned int iy, unsigned int iz, unsigned int alpha,  struct H_parameters &Hp, struct Node* Site){
+
+    double h_Kinetic=0., h_Josephson=0., h_AB=0., h_tot;
+    double h2=(Hp.h*Hp.h);
+    double J_alpha1, J_beta1, J_alpha2, J_beta2;
+    double gauge_phase1, gauge_phase2;
+    unsigned int beta, vec, i;
+
+    i=ix +Lx*(iy+Ly*iz);
+    //We need to compute just the part of the Hamiltonian involving Psi.t
+
+    //Kinetic= -(1/h²)*\sum_k=1,2,3 (|Psi_{alpha}(r)||Psi_{alpha}(r+k)|* cos(theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r))) + (|Psi_{alpha}(r-k)||Psi_{alpha}(r)|* cos(theta_{alpha}(r) - theta_{alpha}(r-k) +h*e*A_k(r-k)))
+    for(vec=0; vec<3; vec++){
+
+        gauge_phase1=Site[nn(i, vec, 1)].Psi[alpha].t - Psi.t + Hp.h*Hp.e*Site[i].A[vec];
+        gauge_phase2=Psi.t -Site[nn(i, vec, -1)].Psi[alpha].t + Hp.h*Hp.e*Site[nn(i, vec, -1)].A[vec];
+        h_Kinetic-=(1./h2)*(Psi.r*Site[nn(i, vec, 1)].Psi[alpha].r)*cos(gauge_phase1);
+        h_Kinetic-=(1./h2)*(Psi.r*Site[nn(i, vec, -1)].Psi[alpha].r)*cos(gauge_phase2);
+
+       //Andreev-Bashkin term = \sum_beta!=alpha \sum_k=1,2,3 -nu*J^k_alpha*J^k_beta;
+        // with J^k_alpha= |Psi_{alpha}(r)||Psi_{alpha}(r+k)|* sin(theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r)))
+        if(Hp.nu !=0 ) {
+
+        J_alpha1= (1./Hp.h)*(Psi.r*Site[nn(i, vec, 1)].Psi[alpha].r)*sin(gauge_phase1);
+        J_alpha2= (1./Hp.h)*(Psi.r*Site[nn(i, vec, -1)].Psi[alpha].r)*sin(gauge_phase2);
+
+        for (beta = 0; beta < 3; beta++) {
+                if (beta != alpha) {
+                J_beta1=(1./Hp.h)*(Site[i].Psi[beta].r*Site[nn(i, vec, 1)].Psi[beta].r)*sin(Site[nn(i, vec, 1)].Psi[beta].t - Site[i].Psi[beta].t + Hp.h*Hp.e*Site[i].A[vec]);
+                h_AB -= Hp.nu *(J_alpha1*J_beta1);
+                J_beta2=(1./Hp.h)*(Site[i].Psi[beta].r*Site[nn(i, vec, -1)].Psi[beta].r)*sin( Site[i].Psi[beta].t -Site[nn(i, vec, -1)].Psi[beta].t + Hp.h*Hp.e*Site[nn(i, vec, -1)].A[vec]);
+                h_AB -= Hp.nu * (J_alpha2 *J_beta2);
+                	}
+        	}
+        }
+    }
+
+    //Josephson= eta* \sum_beta!=alpha |Psi_{alpha}(r)||Psi_{beta}(r)|* cos(theta_{alpha}(r) - theta_{beta}(r))
+    for(beta=0; beta<3; beta++){
+	if(beta != alpha) {
+            h_Josephson += (Hp.eta * O2prod(Psi, Site[i].Psi[beta]));
+        }
+    }
+
+    h_tot= h_Kinetic + h_Josephson + h_AB;
+    return h_tot;
+}
+
+double local_HA(double A, unsigned int ix, unsigned int iy, unsigned int iz,  unsigned int vec,  struct H_parameters &Hp, struct Node* Site){
+
+    double h_Kinetic=0., h_B, h_AB=0., h_tot;
+    double h2=(Hp.h*Hp.h);
+    double J_alpha, J_beta, J_gamma;
+    unsigned int alpha, i;
+    std::vector<struct O2> aux_field(3);
+    i=ix +Lx*(iy+Ly*iz);
+
+    for(alpha=0; alpha<3; alpha++) {
+        aux_field[alpha].r =Site[nn(i, vec, 1)].Psi[alpha].r;
+        aux_field[alpha].t =Site[nn(i, vec, 1)].Psi[alpha].t + Hp.h * Hp.e * A;
+        polar_to_cartesian(aux_field[alpha]);
+    }
+
+    //Compute the local Energy respect to a given component (alpha) of the vector potential A and a given spatial position (r=(ix, iy, iz))
+    //We need to compute just the part of the Hamiltonian involving A
+
+    //Kinetic= -(1/h²)*\sum_k=1,2,3 |Psi_{alpha}(r)||Psi_{alpha}(r+k)|* cos(theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r))
+    for(alpha=0; alpha<3; alpha++) {
+        h_Kinetic -= (1. / h2) * O2prod(Site[i].Psi[alpha], aux_field[alpha]);
+	 }
+    //Andreev-Bashkin term = \sum_beta!=alpha \sum_k=1,2,3 -nu*(J^k_alpha*J^k_beta);
+    // with J^k_alpha= |Psi_{alpha}(r)||Psi_{alpha}(r+k)|* sin(theta_{alpha}(r+k) - theta_{alpha}(r) +h*e*A_k(r)))
+    if(Hp.nu !=0 ) {
+
+        J_alpha=(1. / Hp.h) * O2vprod(Site[i].Psi[0], aux_field[0]);
+        J_beta=(1. / Hp.h) * O2vprod(Site[i].Psi[1], aux_field[1]);
+        J_gamma=(1. / Hp.h) * O2vprod(Site[i].Psi[2], aux_field[2]);
+
+            h_AB -= Hp.nu * ((J_alpha *J_beta)  + (J_alpha*J_gamma) + (J_gamma* J_beta));
+    }
+
+    h_B=(0.5/h2)*F_2(A, vec, ix, iy, iz, Site);
+
+    h_tot= h_Kinetic + h_AB+ h_B;
+    return h_tot;
+}
+
+double F_2(double newA, unsigned int k, unsigned int ix, unsigned int iy, unsigned int iz, struct Node* Site){
+
+    unsigned int l;
+    unsigned int i;
+    double F2_A=0., F_A;
+    i=ix +Lx*(iy+Ly*iz);
+    //All the plaquettes involving A_vec(i)
+    for(l=0; l<3; l++){
+        if(l!= k){
+            F_A=(newA + Site[nn(i, k, 1)].A[l] - Site[nn(i, l, 1)].A[k] - Site[i].A[l]);
+            F2_A+=(F_A*F_A);
+        }
+    }
+    for(l=0; l<3; l++){
+        if(l!= k){
+            F_A=(Site[nn(i, l, -1)].A[k]+ Site[nn(nn(i, l, -1), k, 1)].A[l] - newA - Site[nn(i, l, -1)].A[l]);
+            F2_A+=(F_A*F_A);
+        }
+    }
+
+    return F2_A;
+}
+
